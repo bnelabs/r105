6e477c862fab1ed9b3dd3f452dd72a10784a4ff7
Rahat <rahatimrahat@gmail.com>
2026-09-14 19:09:41 +0300

    Sync Homebrew and Scoop metadata to v1.1.0

--- docs/homebrew.rb
+++ docs/homebrew.rb
@@ -1,26 +1,26 @@
 class R105 < Formula
   desc "Native local-first AI harness for OpenAI-compatible backends"
   homepage "https://github.com/bnelabs/r105"
-  version "1.0.2"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bnelabs/r105/releases/download/v1.0.2/r105-macos-arm64.tar.gz"
-      sha256 "e685d9cc3d0e90bfcd16d7b3af1437ac73a6170c5ec3ee0e246a0ddfe796f8ce"
+      url "https://github.com/bnelabs/r105/releases/download/v1.1.0/r105-macos-arm64.tar.gz"
+      sha256 "36070a28d113a2e007495d7d2233d2ce8232b9a1b7012fa9f573b607c7d7f102"
     else
-      url "https://github.com/bnelabs/r105/releases/download/v1.0.2/r105-macos-x86_64.tar.gz"
-      sha256 "7f17ca24dfeb925b6d35b4b12513ac9fa1f06d729e670572da42776f49e9280b"
+      url "https://github.com/bnelabs/r105/releases/download/v1.1.0/r105-macos-x86_64.tar.gz"
+      sha256 "d65ede4874f60b19bc84f99ad26a23671d8953f9e9b12f0e8644eff32dc58225"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bnelabs/r105/releases/download/v1.0.2/r105-linux-aarch64.tar.gz"
-      sha256 "33380be97917961938416a9297048a765e99302e915bad7560023e1d97cf7336"
+      url "https://github.com/bnelabs/r105/releases/download/v1.1.0/r105-linux-aarch64.tar.gz"
+      sha256 "b50ddcc201e35fb80077360d9341e5895c9c25ffe9496b04d691f740bbf7d73b"
     else
-      url "https://github.com/bnelabs/r105/releases/download/v1.0.2/r105-linux-x86_64.tar.gz"
-      sha256 "f9e84f640a393edd48cf6438e99f5f86cc4872fb5f88849596a8c621a56927dd"
+      url "https://github.com/bnelabs/r105/releases/download/v1.1.0/r105-linux-x86_64.tar.gz"
+      sha256 "c9dee6e2f45371c2d2f6488c267d0f851758ca73c2a257e13b5a4a21c1a918ad"
     end
   end
 
